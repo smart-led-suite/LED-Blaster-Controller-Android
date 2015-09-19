a7dc73be466e78d6898c1578e94dfb175d1baf22
Daniel Bücheler <dbahn25@t-online.de>
2015-09-19 22:42:41 +0200

    MAJOR UPDATE: - redesigned interface - added support for continuous fade modes

--- java/de/danielbuecheler/ledblastercontroller/MainActivity.java
+++ java/de/danielbuecheler/ledblastercontroller/MainActivity.java
@@ -14,8 +14,11 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -24,8 +27,9 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.concurrent.ExecutionException;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, SeekBar.OnSeekBarChangeListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, SeekBar.OnSeekBarChangeListener {
     final String TAG = "MainActivity";
+    private static final int TIME_CHANGE_FACTOR = 50;
 
     boolean[] cb_states = new boolean[4];
     int[] sb_states = new int[4];
@@ -67,9 +71,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
         // ##### SET ON CLICK LISTENERS #####
-        // initialize apply button
-        Button btn_apply = (Button) findViewById(R.id.btn_apply);
-        btn_apply.setOnClickListener(this);
+        // initialize buttons
+        // apply button
+        Button btn;
+        btn = (Button) findViewById(R.id.btn_apply);
+        btn.setOnClickListener(this);
+        // continuous fade buttons
+        btn = (Button) findViewById(R.id.btn_mode1);
+        btn.setOnClickListener(this);
+        btn = (Button) findViewById(R.id.btn_mode2);
+        btn.setOnClickListener(this);
+        btn = (Button) findViewById(R.id.btn_stop_fademode);
+        btn.setOnClickListener(this);
+
+        // increment / decrement buttons for time
+        ImageButton i_btn;
+        i_btn = (ImageButton) findViewById(R.id.btn_inc_time);
+        i_btn.setOnClickListener(this);
+        i_btn = (ImageButton) findViewById(R.id.btn_dec_time);
+        i_btn.setOnClickListener(this);
 
         // initialize all CheckBoxes
         CheckBox cb;
@@ -92,16 +112,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         sb = (SeekBar) findViewById(R.id.sb_blue);
         sb.setOnSeekBarChangeListener(this);
         onProgressChanged(sb, 0, false);
+
+
+
 }
 
     @Override
     protected void onStart() {
         super.onStart();
         twoFloors = prefs.getBoolean("pref_two_floors", false);
+
         fadetime = Integer.valueOf(prefs.getString("pref_fadetime", "500"));
+
+        // initialize edittext for fade time
+        EditText et = (EditText) findViewById(R.id.et_time);
+        et.setText(String.valueOf(fadetime));
+        et.setSelection(et.getText().toString().length());
     }
-
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -129,45 +156,76 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        if(v.getId() == R.id.btn_apply) { // if the button pressed is the apply button
-            // deactivate the Button while fading...
-            Button btn = (Button) v; // we have to cast it to a button so we can call setActivated()
-            btn.setActivated(false);
+        EditText et;
+        int button = 2; // which fademode button is pressed
+        switch(v.getId()) {
+            case R.id.btn_apply: // if the button pressed is the apply button
+                // deactivate the Button while fading...
+                Button btn = (Button) v; // we have to cast it to a button so we can call setActivated()
+                btn.setActivated(false);
 
-            CheckBox cb; // var for the checkboxes i have to check in a second
+                CheckBox cb; // var for the checkboxes i have to check in a second
 
-            // set the val to -1 if checkbox is NOT checked
-            cb = (CheckBox) findViewById(R.id.cb_white);
-            if(!cb.isChecked())
-                sb_states[0] = -1;
-            cb = (CheckBox) findViewById(R.id.cb_red);
-            if(!cb.isChecked())
-                sb_states[1] = -1;
-            cb = (CheckBox) findViewById(R.id.cb_green);
-            if(!cb.isChecked())
-                sb_states[2] = -1;
-            cb = (CheckBox) findViewById(R.id.cb_blue);
-            if(!cb.isChecked())
-                sb_states[3] = -1;
+                // set the val to -1 if checkbox is NOT checked
+                cb = (CheckBox) findViewById(R.id.cb_white);
+                if(!cb.isChecked())
+                    sb_states[0] = -1;
+                cb = (CheckBox) findViewById(R.id.cb_red);
+                if(!cb.isChecked())
+                    sb_states[1] = -1;
+                cb = (CheckBox) findViewById(R.id.cb_green);
+                if(!cb.isChecked())
+                    sb_states[2] = -1;
+                cb = (CheckBox) findViewById(R.id.cb_blue);
+                if(!cb.isChecked())
+                    sb_states[3] = -1;
 
-            Log.d(TAG, "LED_states:");
-            for(int val : sb_states) {
-                Log.d(TAG, String.valueOf(val));
-            }
+                Log.d(TAG, "LED_states:");
+                for(int val : sb_states) {
+                    Log.d(TAG, String.valueOf(val));
+                }
 
-            QueryLEDsTask fade_task = new QueryLEDsTask();
-            fade_task.execute(String.valueOf(sb_states[0]), String.valueOf(sb_states[1]), String.valueOf(sb_states[2]), String.valueOf(sb_states[3]));
+                SteadyFadeTask fade_task = new SteadyFadeTask();
+                fade_task.execute(String.valueOf(sb_states[0]), String.valueOf(sb_states[1]), String.valueOf(sb_states[2]), String.valueOf(sb_states[3]));
 
-            try {
-                Log.d(TAG, String.valueOf(fade_task.get()));
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } catch (ExecutionException e) {
-                e.printStackTrace();
-            }
+                try {
+                    Log.d(TAG, String.valueOf(fade_task.get()));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                }
 
+                resetUI();
+                break;
 
-            resetUI();
+            case R.id.btn_mode1: // button pressed == btn for mode 1
+                button = 1;
+            case R.id.btn_mode2:
+                et = (EditText) findViewById(R.id.et_time);
+                int fadetime_continuous = Integer.valueOf(et.getText().toString());
+
+                // take focus from et
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(et.getWindowToken(), 0);
+
+                SetModeTask set_mode_task = new SetModeTask();
+                set_mode_task.execute(button, fadetime_continuous); // start fade mode 1
+                break;
+            case R.id.btn_stop_fademode:
+                SteadyFadeTask task = new SteadyFadeTask();
+                task.execute("0", "0", "0", "0");
+                break;
+            case R.id.btn_inc_time:
+                et = (EditText) findViewById(R.id.et_time);
+                et.setText(String.valueOf(Integer.valueOf(
+                                et.getText().toString()) + TIME_CHANGE_FACTOR));
+                break;
+            case R.id.btn_dec_time:
+                et = (EditText) findViewById(R.id.et_time);
+                et.setText(String.valueOf(Integer.valueOf(
+                        et.getText().toString()) - TIME_CHANGE_FACTOR));
+                break;
         }
     }
 
@@ -267,27 +325,51 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
-    private class QueryLEDsTask extends AsyncTask<String, Void, Integer> {
+    private class SteadyFadeTask extends AsyncTask<String, Void, Integer> {
         @Override
         protected Integer doInBackground(String... led_vals) {
             int response = 404;
             try {
-                Log.d(TAG, "starting network request");
                 String url_http = prefs.getString("pref_url", "").trim(); // remove whitespaces at beginning and end
-                Log.d(TAG, url_http);
+                String url_get;
+                String url_complete;
+
+                URL url;
+                HttpURLConnection con;
+
                 if(!url_http.endsWith("?")) // add "?" to begin GET if not already existing
                     url_http = url_http.concat("?");
                 if(!url_http.startsWith("http://"))
                     url_http = "http://".concat(url_http); // add "http://" to the beginning
-                String url_get = String.format(getResources().getString(R.string.url_get), led_vals[0], led_vals[1], led_vals[2], led_vals[3], fadetime);
-                Log.d(TAG, url_get);
-                String url_complete = url_http.concat(url_get);
+
+                // TURN MODE 0 ON
+                url_get = String.format(getResources().getString(R.string.url_get_fademode), 0, 1); // set to mode 0 in 1 ms
+                url_complete = url_http.concat(url_get);
                 Log.d(TAG, url_complete);
-                URL url = new URL(url_complete);
-                HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                url = new URL(url_complete);
+                // send request
+                con = (HttpURLConnection) url.openConnection();
                 con.setReadTimeout(10000 /* millis */);
                 con.setConnectTimeout(15000 /* millis */);
                 con.connect();
+                response = con.getResponseCode();
+                Log.d(TAG, "Response code: " + response);
+
+                // FADE LEDs
+                // prepare network request
+                url_get = String.format(getResources().getString(R.string.url_get_steady), led_vals[0], led_vals[1], led_vals[2], led_vals[3], fadetime);
+                url_complete = url_http.concat(url_get);
+
+                Log.d(TAG, url_complete);
+
+                url = new URL(url_complete);
+
+                // send request
+                con = (HttpURLConnection) url.openConnection();
+                con.setReadTimeout(10000 /* millis */);
+                con.setConnectTimeout(15000 /* millis */);
+                con.connect();
+
                 response = con.getResponseCode();
                 Log.d(TAG, "Response code: " + response);
             } catch(Exception e) {
@@ -310,6 +392,52 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             // reactivate button
             findViewById(R.id.btn_apply).setActivated(true);
 
+        }
+    }
+
+    private class SetModeTask extends AsyncTask<Integer, Void, Integer> {
+        @Override
+        protected Integer doInBackground(Integer... params) {
+            int response = 404; // if it fails we have to assume 404 not found
+            try {
+                // prepare network request
+                String url_http = prefs.getString("pref_url", "").trim(); // remove whitespaces at beginning and end
+
+                if(!url_http.endsWith("?")) // add "?" to begin GET if not already existing
+                    url_http = url_http.concat("?");
+                if(!url_http.startsWith("http://"))
+                    url_http = "http://".concat(url_http); // add "http://" to the beginning
+
+                String url_get = String.format(getResources().getString(R.string.url_get_fademode), params[0], params[1]);
+                String url_complete = url_http.concat(url_get);
+
+                URL url = new URL(url_complete);
+
+                // connect to fade.php
+                HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                con.setReadTimeout(10000 /* millis */);
+                con.setConnectTimeout(15000 /* millis */);
+                con.connect();
+
+                response = con.getResponseCode();
+                Log.d(TAG, "Response code: " + response);
+            } catch(Exception e) {
+                Log.d(TAG, e.toString());
+            }
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(Integer result) {
+            if(result >= 200 && result < 300) { // if http status code is 2xx (success)
+                Toast toast = Toast.makeText(getApplicationContext(), getResources().getString(R.string.toast_fademode_started), Toast.LENGTH_SHORT); // show toast that the LEDs are fading
+                toast.show();
+            } else { // if error occurred
+                String message = String.format(getResources().getString(R.string.toast_error_http), result);
+
+                Toast toast = Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG); // show toast that an error occured, including error / status code
+                toast.show();
+            }
         }
     }
 }
