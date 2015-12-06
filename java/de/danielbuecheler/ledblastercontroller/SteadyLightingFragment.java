39af3b632d7bca9cbd8535bcc986230c87a38828
Daniel Bücheler <dbahn25@t-online.de>
2015-12-06 23:01:34 +0100

    split up MainFragment into FadeModesFragment and SteadyFadeFragment and added landscape support

--- java/de/danielbuecheler/ledblastercontroller/SteadyLightingFragment.java
+++ java/de/danielbuecheler/ledblastercontroller/SteadyLightingFragment.java
@@ -2,7 +2,6 @@ package de.danielbuecheler.ledblastercontroller;
 
 import android.app.Fragment;
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.net.ConnectivityManager;
@@ -29,9 +28,8 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 
-public class MainFragment extends Fragment implements View.OnClickListener, SeekBar.OnSeekBarChangeListener {
-    final String TAG = "MainFragment";
-    private static final int TIME_CHANGE_FACTOR = 50;
+public class SteadyLightingFragment extends Fragment implements View.OnClickListener, SeekBar.OnSeekBarChangeListener {
+    final String TAG = "SteadyLightingFragment";
 
 //    ArrayList<Boolean> cb_states = new ArrayList<>();
     ArrayList<Integer> sb_states = new ArrayList<>();
@@ -59,7 +57,7 @@ public class MainFragment extends Fragment implements View.OnClickListener, Seek
         Resources res = getResources();
 
 
-        View returnView = inflater.inflate(R.layout.fragment_main, container, false);
+        View returnView = inflater.inflate(R.layout.fragment_steady_lighting, container, false);
 
         ConnectivityManager connMgr = (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
 
@@ -80,20 +78,8 @@ public class MainFragment extends Fragment implements View.OnClickListener, Seek
         Button btn;
         btn = (Button) returnView.findViewById(R.id.btn_apply);
         btn.setOnClickListener(this);
-        // continuous fade buttons
-        btn = (Button) returnView.findViewById(R.id.btn_mode1);
-        btn.setOnClickListener(this);
-        btn = (Button) returnView.findViewById(R.id.btn_modes_off);
-        btn.setOnClickListener(this);
         btn = (Button) returnView.findViewById(R.id.btn_all_off);
         btn.setOnClickListener(this);
-
-        // increment / decrement buttons for time
-        ImageButton i_btn;
-        i_btn = (ImageButton) returnView.findViewById(R.id.btn_inc_time);
-        i_btn.setOnClickListener(this);
-        i_btn = (ImageButton) returnView.findViewById(R.id.btn_dec_time);
-        i_btn.setOnClickListener(this);
 
         adapter = new LEDItemAdapter(this.getActivity());
 
@@ -132,11 +118,6 @@ public class MainFragment extends Fragment implements View.OnClickListener, Seek
         twoFloors = prefs.getBoolean("pref_two_floors", false);
 
         fadetime = Integer.valueOf(prefs.getString("pref_fadetime", "500"));
-
-        // initialize edittext for fade time
-        EditText et = (EditText) getView().findViewById(R.id.et_time);
-        et.setText(String.valueOf(fadetime));
-        et.setSelection(et.getText().toString().length());
     }
 
 //    @Override
@@ -207,23 +188,7 @@ public class MainFragment extends Fragment implements View.OnClickListener, Seek
                 resetUI();
                 break;
 
-            case R.id.btn_mode1: // button pressed == btn for mode 1
-                button = 1;
 
-                et = (EditText) getView().findViewById(R.id.et_time);
-                int fadetime_continuous = Integer.valueOf(et.getText().toString());
-
-                // take focus from et
-                InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
-                imm.hideSoftInputFromWindow(et.getWindowToken(), 0);
-
-                SetModeTask set_mode_task = new SetModeTask();
-                set_mode_task.execute(1, button, fadetime_continuous); // start fade mode 1 ([0]=1 for turning fade mode on
-                break;
-            case R.id.btn_modes_off:
-                set_mode_task = new SetModeTask();
-                set_mode_task.execute(0); // 2500 is placeholder, not needed
-                break;
             case R.id.btn_all_off:
                 task = new SteadyFadeTask();
                 for(int item = 0; item < adapterCount; item++) {
@@ -231,16 +196,7 @@ public class MainFragment extends Fragment implements View.OnClickListener, Seek
                 }
                 task.execute(targets);
                 break;
-            case R.id.btn_inc_time:
-                et = (EditText) getView().findViewById(R.id.et_time);
-                et.setText(String.valueOf(Integer.valueOf(
-                                et.getText().toString()) + TIME_CHANGE_FACTOR));
-                break;
-            case R.id.btn_dec_time:
-                et = (EditText) getView().findViewById(R.id.et_time);
-                et.setText(String.valueOf(Integer.valueOf(
-                        et.getText().toString()) - TIME_CHANGE_FACTOR));
-                break;
+
         }
     }
 
@@ -369,60 +325,6 @@ public class MainFragment extends Fragment implements View.OnClickListener, Seek
             // reactivate button
             getView().findViewById(R.id.btn_apply).setActivated(true);
 
-        }
-    }
-
-    private class SetModeTask extends AsyncTask<Integer, Void, Integer> {
-        @Override
-        protected Integer doInBackground(Integer... params) {
-            int response = 404; // if it fails we have to assume 404 not found
-            try {
-                // prepare network request
-                String url_http = prefs.getString("pref_url", "").trim(); // remove whitespaces at beginning and end
-
-                if(!url_http.endsWith("?")) // add "?" to begin GET if not already existing
-                    url_http = url_http.concat("?");
-                if(!url_http.startsWith("http://"))
-                    url_http = "http://".concat(url_http); // add "http://" to the beginning
-
-
-                String url_get;
-                if(params[0] != 0) {
-                    url_get = String.format(getResources().getString(R.string.url_get_fademode), params[1], params[2]);
-                } else {
-                    url_get = String.format(getResources().getString(R.string.url_get_fademode), 0, 0);
-                }
-                String url_complete = url_http.concat(url_get);
-
-                Log.d(TAG, url_complete);
-
-                URL url = new URL(url_complete);
-
-                // connect to fade.php
-                HttpURLConnection con = (HttpURLConnection) url.openConnection();
-                con.setReadTimeout(10000 /* millis */);
-                con.setConnectTimeout(15000 /* millis */);
-                con.connect();
-
-                response = con.getResponseCode();
-                Log.d(TAG, "Response code: " + response);
-            } catch(Exception e) {
-                Log.d(TAG, e.toString());
-            }
-            return response;
-        }
-
-        @Override
-        protected void onPostExecute(Integer result) {
-            if(result >= 200 && result < 300) { // if http status code is 2xx (success)
-                Toast toast = Toast.makeText(getActivity().getApplicationContext(), getResources().getString(R.string.toast_fademode_started), Toast.LENGTH_SHORT); // show toast that the LEDs are fading
-                toast.show();
-            } else { // if error occurred
-                String message = String.format(getResources().getString(R.string.toast_error_http), result);
-
-                Toast toast = Toast.makeText(getActivity().getApplicationContext(), message, Toast.LENGTH_LONG); // show toast that an error occured, including error / status code
-                toast.show();
-            }
         }
     }
 }
